66876822bccb0a3ed10acca4ee1f1404fe3370e2
CURSO GESTÃO TI - ESTÁCIO <th.costa.lima@gmail.com>
2025-09-21 16:54:17 +0000

    continuação das regras e comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,34 +17,43 @@ int main() {
 printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 printf("*****Olá, você está no SUPER TRUNFO - Países!******\n");
-printf("+++++++++++++++++++++++++++++++++++++++++++++++++++ \n");
+printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 printf("!!!!!!!!  ATENÇÃO PARA AS REGRAS DO JOGO   !!!!!!!!\n");
-printf(" De Olho nas Regras!! \n");
-printf("...");
-printf("Você deverá fornecer as seguintes informações: \n");
 printf("\n");
-  printf("Estado");  
-    printf("Você deve escolher um Estado:... \n ");
-      printf("...após escolher o Estado, escolha uma lerta entre A e H que irá representar esse Estado, ex:. Bahia(B) \n");
+printf("*************** De Olho nas Regras *****************\n");
+printf("\n");
+printf("Digite o Estado de sua escolha... \n ");
+  printf("Estado: "); 
+    //linha de código para o usuário inserir o Estado
+    printf("Estado: ", &Estado);
+      printf(" - * após escolher o Estado, escolha uma lerta entre A e H que irá representar esse Estado, ex:. Bahia(B) \n");
+        printf("\n");
+printf("Código da Carta ");
+  printf("ATENÇÃO!!, O código da carte deve conter a letra que irá representar o Estado seguida de um número, ex:. Estado= São Paulo_letra_A_Cod: A01; \n");
+      //linha com código para usuário inserir o Código
+      printf("Código: ", &Codigo);
+        printf("\n");      
+printf("Cidade");
+  printf("escolha uma cidade pertencente ao Estado que escolheu anteriormente, ex:. Estado Bahia_cidade_Ilhéus;\n");
+  //linha com código para usuário inserir a Cidade
+  printf("Cidade")
+    printf("\n");
+printf("População: ");
+  printf("insira a Populção aproximada da cidade escolhida anteriormente; \n");
+  //linha com código para usuário inserir População
+    printf("\n");
+printf("Área: ");
+  printf("insira um valor aproximado da densidade demgráfica da CIDADE escolhida anteriormente; \n");
+  //linha com código para usuário inserir densidade demográfica
       printf("\n");
-  printf("Código da Carta: ");
-    printf("ATENÇÃO!!, O código da carte deve conter a letra que irá representar o Estado seguida de um número, ex:. Estado Bahia_letra_B_Cod: B01; \n");
+printf("PIB:");
+  printf("insira um valor aproximado do PIB da CIDADE escolhida anteriormente; \n");
+  //linha com código para usuário inserir PIB
     printf("\n");
-  printf("CIDADE: ");
-    printf("escolha uma cidade pertencente ao Estado que escolheu anteriormente, ex:. Estado Bahia_cidade_Ilhéus;\n");
-    printf("\n");
-  printf("POPULAÇÃO: ");
-    printf("insira Populção aproximada da cidade escolhida anteriormente; \n");
-    printf("\n");
-  printf("ÁREA: ");
-    printf("insira um valor aproximado da densidade demgráfica da CIDADE escolhida anteriormente; \n");
-    printf("\n");
-  printf("PIB:");
-    printf("insira um valor aproximado do PIB da CIDADE escolhida anteriormente; \n");
-    printf("\n");
-  printf("PONTOS TURÍSTICOS:");
-    printf("insira um valor aproximado dos pontos turísticos da CIDADE escolhida anteriormente; \n");
+printf("Pontos Turísticos:");
+  printf("insira um valor aproximado dos pontos turísticos da CIDADE escolhida anteriormente; \n");
+  //linha com código para usuário inserir PIB
     printf("\n");
   
   // Área para entrada de dados
