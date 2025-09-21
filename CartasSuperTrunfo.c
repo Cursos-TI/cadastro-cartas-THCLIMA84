8548ce5db7da2378ab2cdcd9b00237daa9041b8d
CURSO GESTÃO TI - ESTÁCIO <th.costa.lima@gmail.com>
2025-09-21 20:36:38 +0000

    introdução de entrada de dados_1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,11 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     int População;
     char Estado;
-    char Codigo;
+    char Código;
     char Cidade[20];
     float Área;
     float PIB;
-    int PTurísmo;
+    int PTurístico;
 
     printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
     printf("+++++++++++++++++++++++++++++++++++++++++++++++++++\n");
@@ -23,41 +23,45 @@ int main() {
     printf("\n");
     printf("*************** De Olho Nas Regras *****************\n");
     printf("\n");
-    printf("Digite um Estado de sua Escolha");
+    printf("Para começarmos, digite um Estado de sua Escolha \n");
       scanf("%s", &Estado);
-      printf("Estado: %s", &Estado);
-      printf(" - * Após escolher o Estado, escolha uma lerta entre A e H que irá representar esse Estado, ex:. Bahia(B) \n");
-      printf("\n");
-    printf("Código da Carta ");
-      printf("ATENÇÃO!!, O código da carta deve conter a letra que irá representar o Estado seguida de um número, ex. 1: Estado= São Paulo_letra_A_Código: A01; ex. 2: Bahia_letra_B_Código: B1 \n");
-      printf("Código: ", &Codigo);
-        //linha com código para usuário inserir o Código
-          printf("\n");      
-    printf("Cidade");
+        printf("Estado: %s \n", &Estado );
+          printf(" - * Após escolher o Estado, escolha uma lerta entre A e H que será Código da carta que irá representar esse Estado, ex:. Bahia(B) \n");
+            printf("\n");
+    printf("Código da Carta \n");
+      printf("- *ATENÇÃO!!* \n");
+        printf(" O código da carta deve conter a letra que irá representar o Estado seguida de um número, ex. 1: Estado= São Paulo_letra_A_Código: A01; ex. 2: Bahia_letra_B_Código: B1 \n");
+          printf("\n");
+          printf("Digite o Código da carta \n ");
+            scanf("%s", &Código);              C
+              printf("Código: %i", &Código);
+                printf("\n");      
+    printf("Cidade \n");
       printf("Escolha uma cidade pertencente ao Estado que escolheu anteriormente, ex:. Estado Bahia_cidade_Ilhéus;\n");
-      printf("Cidade", &Cidade);
-        //linha com código para usuário inseri a Cidade
+       printf("\n");
+        scanf("%s ", &Cidade);  
+         printf("Cidade %s", &Cidade);
           printf("\n");
-  printf("População");
-    printf("Insira a Populção aproximada da cidade escolhida anteriormente; \n");
-      printf("População)", &População);
-        //linha com código para usuário inserir População
-          printf("\n");
-printf("Área ");
-  printf("insira um valor aproximado da densidade demgráfica da CIDADE escolhida anteriormente; \n");
-    printf("Área: ", &Área);  
-      //linha com código para usuário inserir densidade demográfica
-        printf("\n");
-printf("PIB:");
-  printf("insira um valor aproximado do PIB da CIDADE escolhida anteriormente; \n");
-    printf("PIB: ", &PIB);
-      //linha com código para usuário inserir PIB
-        printf("\n");
-printf("Pontos Turísticos:");
-  printf("insira um valor aproximado dos pontos turísticos da CIDADE escolhida anteriormente; \n");
-    printf("Pontos Turísticos: ", &PTurísmo);  
-      //linha com código para usuário inserir PIB
-        printf("\n");
+    printf("População \n");
+      printf("Insira a Populção aproximada da cidade escolhida anteriormente; \n");
+        scanf("%f", &População);
+          printf("População)", &População);        
+            printf("\n");
+    printf("Área \n");
+      printf("insira um valor aproximado da densidade demgráfica da CIDADE escolhida anteriormente; \n");
+        scanf("%f" , &Área);
+          printf("Área: %f", &Área);      
+            printf("\n");
+    printf("PIB \n");
+      printf("insira um valor aproximado do PIB da CIDADE escolhida anteriormente; \n");
+        scanf("%f", &PIB);
+          printf("PIB: %f ", &PIB);      
+            printf("\n");
+    printf("Pontos Turísticos \n");
+      printf("insira um valor aproximado dos pontos turísticos da CIDADE escolhida anteriormente; \n");
+        scanf("%i", & PTurístico);
+          printf("Pontos Turísticos: %i", &PTurístico); 
+            printf("\n");
   
   
   // Área para exibição dos dados da cidade
