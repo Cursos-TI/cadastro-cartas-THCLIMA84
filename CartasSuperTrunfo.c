9607e31eecb38bb4543b6aee7af0e8f246c2eaa9
CURSO GESTÃO TI - ESTÁCIO <th.costa.lima@gmail.com>
2025-09-28 17:49:32 +0000

    Introdução interação com usuário_2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     int População;
     char Estado;
-    char Código;
+    char Código[20];
     char Cidade[20];
     float Área;
     float PIB;
@@ -33,20 +33,21 @@ int main() {
         printf(" O código da carta deve conter a letra que irá representar o Estado seguida de um número, ex. 1: Estado= São Paulo_letra_A_Código: A01; ex. 2: Bahia_letra_B_Código: B1 \n");
           printf("\n");
           printf("Digite o Código da carta \n ");
-            scanf("%s", &Código);              C
-              printf("Código: %i", &Código);
-                printf("\n");      
+            scanf("%s", &Código);
+              printf("Código: %s \n ", &Código);                
+                  Código[20] = &Código;
+                    printf("\n");      
     printf("Cidade \n");
       printf("Escolha uma cidade pertencente ao Estado que escolheu anteriormente, ex:. Estado Bahia_cidade_Ilhéus;\n");
-       printf("\n");
-        scanf("%s ", &Cidade);  
-         printf("Cidade %s", &Cidade);
-          printf("\n");
+        printf("Digite a Cidade: ");
+          scanf("%s ", &Cidade);              
+            printf("\n");
     printf("População \n");
       printf("Insira a Populção aproximada da cidade escolhida anteriormente; \n");
-        scanf("%f", &População);
-          printf("População)", &População);        
-            printf("\n");
+        printf("Digite a População: ");
+          scanf("%f", &População);
+            printf("População)", &População);        
+              printf("\n");
     printf("Área \n");
       printf("insira um valor aproximado da densidade demgráfica da CIDADE escolhida anteriormente; \n");
         scanf("%f" , &Área);
